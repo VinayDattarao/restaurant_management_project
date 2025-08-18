55db034f66327bef5069ff6f4d014413546b7115
Vinay Datta <vinaydattarao@gmail.com>
2025-08-18 17:09:15 +0530

    34555

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Restaurant 
+from .models import Restaurant
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
-    # At least one option should be inside the class
-    list_display = ('name','phone_number')   # shows the restaurant name in the admin table
+    list_display = ('name','phone_number')
