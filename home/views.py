da76b26c8b476463a238500bd8f81fd49be72087
Vinay Datta <vinaydattarao@gmail.com>
2025-08-18 17:09:49 +0530

    34555

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Restaurant
 
-
 def home(request):
     restaurant = Restaurant.objects.first()
     restaurant_name = restaurant.name if restaurant else "My Restaurant"
@@ -24,12 +23,10 @@ def menu(request):
     {'name': 'Butter Chicken', 'price': 275},
     {'name': 'Paneer Butter Masala', 'price': 250},
     {'name': 'Veg Manchuria', 'price': 150},
-    
-    # Biryani
     {'name': 'Hyderabadi Chicken Biryani', 'price': 210},
     {'name': 'Paneer Biryani', 'price': 185},
     ]
-    
+
     context = {'menu_items': menu_items}
     return render(request, 'menu.html', context)
 
