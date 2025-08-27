8b3672b5cdc653558560a12e150211696661ff20
Vinay Datta <vinaydattarao@gmail.com>
2025-08-27 23:47:08 +0530

    39904

--- home/admin.py
+++ home/admin.py
@@ -4,3 +4,17 @@ from .models import Restaurant
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name','phone_number')
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer_name", "created_at")
+    search_fields = ("customer_name",)
+    ordering = ("-created_at",)
+    filter_horizontal = ("items",)  # nice UI for selecting multiple dishes
