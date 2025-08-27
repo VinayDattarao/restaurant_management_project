7a6ae5e7967e9f7b2b8d9055e8ce6a5517ec46c6
Vinay Datta <vinaydattarao@gmail.com>
2025-08-27 23:44:09 +0530

    39905

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path("contact/", views.contact, name="contact"),
     path('', include('feedback.urls')),
+    path('api/menu/', MenuView.as_view(), name='menu'),
 ]
