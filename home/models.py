8eef259ac5f16b7217a2c644fdaef55f15fb65dd
Vinay Datta <vinaydattarao@gmail.com>
2025-08-27 23:54:55 +0530

    39901

--- home/models.py
+++ home/models.py
@@ -44,3 +44,15 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.customer_name}"
+
+class UserProfile(models.Model):
+    # Link to built-in User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+    # Extra fields
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
