b425366379edc7c27931756088dfa149bd899daa
Vinay Datta <vinaydattarao@gmail.com>
2025-08-27 23:46:45 +0530

    39904

--- home/models.py
+++ home/models.py
@@ -13,3 +13,20 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.comment[:50]
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    items = models.ManyToManyField(Menu)  # an order can have multiple menu items
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
